7364e626f10eb1015cc93e115c9f56e334d27294
梁爽 <liangshuang@192.168.0.116>
2026-08-27 11:08:17 +0800

    feat(user): user 模块开发

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,48 +1,39 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Put,
-  Delete,
-  Body,
-  Param,
-  Query,
-} from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param, Query } from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { CreateUserDto, UpdateUserDto, QueryUserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {}
 
-  @Post('create')
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
+    @Post('create')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.createUser(createUserDto);
+    }
 
-  @Get('list')
-  getUsers() {
-    return this.userService.getUsers();
-  }
+    @Get('list')
+    getUsers() {
+        return this.userService.getUsers();
+    }
 
-  @Get('detail/:id')
-  getUser(@Param('id') id: string) {
-    return this.userService.getUser(id);
-  }
+    @Get('detail/:id')
+    getUser(@Param('id') id: string) {
+        return this.userService.getUser(id);
+    }
 
-  @Delete('delete/:id')
-  deleteUser(@Param('id') id: string) {
-    return this.userService.deleteUser(id);
-  }
+    @Delete('delete/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.userService.deleteUser(id);
+    }
 
-  @Put('update/:id')
-  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.updateUser(id, updateUserDto);
-  }
+    @Put('update/:id')
+    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.updateUser(id, updateUserDto);
+    }
 
-  @Get('search')
-  searchUsers(@Query() query: QueryUserDto) {
-    return this.userService.searchUsers(query);
-  }
+    @Get('search')
+    searchUsers(@Query() query: QueryUserDto) {
+        return this.userService.searchUsers(query);
+    }
 }
