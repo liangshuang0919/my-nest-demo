6232fb05663096b3ffce4ad8b3f87aeb99876d5c
梁爽 <liangshuang@192.168.0.116>
2026-08-27 11:09:18 +0800

    feat(chains): chains 模块开发

---
+++ src/chains/dto/chains.dto.ts
@@ -0,0 +1,12 @@
+export class PolishDto {
+    article: string; // 文章
+}
+
+export class BlogDto {
+    keywords: string; // 关键词
+    style: string; // 风格
+}
+
+export class CategoryDto {
+    question: string; // 问题
+}
